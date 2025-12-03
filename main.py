165513c8042c03769aa291e66ab78ff75a8edeb6
garrett16r <garrett16r@gmail.com>
2025-12-03 15:48:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,214 @@
+import sys, requests, json
+from pathlib import Path
+
+# create local cache folder
+# check command line arg for pokemon name
+# check local cache for json file
+# reach out to API if cache misses
+# store results in cache folder
+# display pokedex info
+
+# TODO:
+# Add separate .py file for computing weaknesses/resistances
+# - Confirm logic behind more complex dual-type interactions
+# - How does one nullifying match-up mix with one super-effective match-up? Neutral or not very effective?
+
+api_pokemon_url = "https://pokeapi.co/api/v2/pokemon"
+api_type_url = "https://pokeapi.co/api/v2/type"
+
+def init() -> tuple[Path, Path]:
+    script_dir = Path(__file__).resolve().parent
+    cache_dir = script_dir / "cache"
+    types_dir = script_dir / "types"
+    pokemon_names_file = cache_dir / "pokemon_names.txt"
+
+    # Create cache folder and download list of all valid Pokemon names
+    if not cache_dir.exists():
+        print("INFO: Cache folder not found. Creating...")
+        cache_dir.mkdir(parents=True)
+        print(f"INFO: Cache folder created at {cache_dir}")
+
+        print("INFO: Downloading list of Pokemon names...")
+        params = {"limit": 10000, "offset": 0}
+        headers = {"Accept": "application/json"}
+        try:
+            response = requests.get(api_pokemon_url, params=params, headers=headers)
+            response.raise_for_status()
+            pokemon_all_raw = response.json()
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+        pokemon_all = []
+
+        for pkmn_obj in pokemon_all_raw["results"]:
+            pokemon_all += pkmn_obj["name"] + "\n"
+
+        with open(pokemon_names_file, "w", encoding="utf-8") as f:
+            f.writelines(pokemon_all)
+            f.close()
+        print("INFO: Download complete!")
+
+    # Create types folder and download json data for all 19 types. Used for type matchup data later
+    if not types_dir.exists():
+        print("INFO: Types folder not found. Creating...")
+        types_dir.mkdir(parents=True)
+        print(f"INFO: Types folder created at {types_dir}")
+
+        print("INFO: Downloading Pokemon type data...")
+        headers = {"Accept": "application/json"}
+
+        for i in range(1,20):
+            try:
+                response = requests.get(f"{api_type_url}/{i}", headers=headers)
+                response.raise_for_status()
+                type_json = response.json()
+                
+            except Exception as e:
+                print(f"ERROR: {e}")
+            
+            type_file = types_dir / f"{type_json["names"][7 if i < 19 else 8]["name"]}.json"
+            with open(type_file, "w", encoding="utf-8") as f:
+                json.dump(type_json, f, ensure_ascii=False, indent=2)
+                f.close()
+
+    return cache_dir, pokemon_names_file
+
+def normalize_name(raw_name: str) -> tuple[str, str]:
+    prefixes = ["mega", "alolan", "galarian", "hisuian", "paldean"]
+    suffixes = ["mega", "alola", "galar", "hisui", "paldea"]
+    name_parts = raw_name.split(" ")
+
+    pretty_pokemon_name = " ".join(part.capitalize() for part in name_parts)
+
+    # Create api_pokemon_name
+    if len(name_parts) > 1 and prefixes.__contains__(name_parts[0].lower()): 
+        api_pokemon_name = f"{name_parts[1]}-{name_parts[0]}"
+
+        name_parts = api_pokemon_name.split("-")
+        suffix = name_parts[1].lower()
+        api_pokemon_name = f"{name_parts[0]}-{suffixes[prefixes.index(suffix)]}"
+    else:
+        api_pokemon_name = name_parts[0].lower()
+        
+        # Replace dots and spaces with hyphens
+        if len(name_parts) > 1: api_pokemon_name = pretty_pokemon_name.replace(". ", "-").replace(" ", "-").lower()
+
+        api_pokemon_name = api_pokemon_name.replace(". ", "-").replace(" ", "-").lower()
+
+    return (api_pokemon_name, pretty_pokemon_name)
+
+def valid_pokemon_name(api_pokemon_name: str, pokemon_names_file: Path) -> bool:
+    with open(pokemon_names_file, "r", encoding="utf-8") as f:
+        for name in f:
+            name = name.rstrip("\n")
+            if name == api_pokemon_name: return True
+
+    return False
+
+def check_cache(api_pokemon_name: str, cache_dir: Path) -> bool:
+    cache_file = cache_dir / f"{api_pokemon_name}.json"
+
+    if cache_file.exists(): return True
+
+    return False
+
+def pull_pokedex_info(api_pokemon_name: str, cache_dir: Path) -> None:
+    cache_file = cache_dir / f"{api_pokemon_name}.json"
+    pokemon_url = f"{api_pokemon_url}/{api_pokemon_name}"
+    headers = {"Accept": "application/json"}
+
+    try:
+        response = requests.get(pokemon_url, headers=headers)
+        response.raise_for_status()
+        pokemon_data = response.json()
+    except Exception as e:
+        print(f"ERROR: {e}")
+        return
+
+    with open(cache_file, "w", encoding="utf-8") as f:
+        json.dump(pokemon_data, f, ensure_ascii=False, indent=2)
+        f.close()
+    return
+
+def show_pokedex_info(api_pokemon_name: str, pretty_pokemon_name: str, cache_dir: Path) -> None:
+
+    # Display:
+    # Name (pkdex#)
+    # Types
+    # Weaknesses?
+    # Resistances?
+    # Abilities (indicate HA)
+    # Sprite links
+    # Cry link
+
+    cache_file = cache_dir / f"{api_pokemon_name}.json"
+
+    with open(cache_file, "r", encoding="utf-8") as f:
+        pokemon_data_json = json.load(f)
+        f.close()
+    
+    dex_num = f"{pokemon_data_json["species"]["url"]}".split("/")[6] # accounts for weird IDs for regional forms
+
+    types = []
+    for i in pokemon_data_json["types"]: types.append(i["type"]["name"])
+    if len(types) == 1: types = f"{types[0].capitalize()} type"
+    else: types = f"{types[0].capitalize()}/{types[1].capitalize()} type"
+
+    abilities = ""
+    for i in pokemon_data_json["abilities"]:
+        if len(pokemon_data_json["abilities"]) == 1: 
+            abilities += f"    {i["ability"]["name"].capitalize()}"
+        else:
+            if i["is_hidden"]: abilities += f"    {i["ability"]["name"].capitalize()} (Hidden)"
+            else: abilities += f"    {i["ability"]["name"].capitalize()}\n"
+
+    sprite_links = []
+    sprite_links.append(pokemon_data_json["sprites"]["other"]["official-artwork"]["front_default"])
+    sprite_links.append(pokemon_data_json["sprites"]["other"]["official-artwork"]["front_shiny"])
+
+    cry_link = pokemon_data_json["cries"]["latest"]
+
+    print(f"{pretty_pokemon_name} (#{dex_num}) - {types}\n",
+          "--------------------------\n",
+          f"Abilities:\n{abilities}\n",
+          "--------------------------\n",
+          f"Normal sprite: {sprite_links[0]}\n",
+          f"Shiny sprite: {sprite_links[1]}\n",
+          "--------------------------\n",
+          f"Cry audio: {cry_link}")
+
+    return
+
+def main():
+    cache_dir, pokemon_names_file = init()
+
+    if len(sys.argv) < 2:
+        print("ERROR: Incomplete command.\n",
+              "Usage: pokedex [pokemon_name]\n",
+              "For megas, use the format \'pokemon-mega\'\n",
+              "For regional forms, use the format \'pokemon-alola\' or \'pokemon-galar\'\n",
+              "Find a full list of pokemon names in ./cache/pokemon_names.txt\n",
+              "Exiting.")
+        return
+    
+    raw_name = " ".join(sys.argv[1:])
+
+    api_pokemon_name, pretty_pokemon_name = normalize_name(raw_name) # api_pokemon_name is formatted for the API call, pretty_pokemon_name is formatted for human readability
+    if not valid_pokemon_name(api_pokemon_name, pokemon_names_file):
+        print(f"ERROR: Invalid Pokemon name: {api_pokemon_name}\n"
+              "For megas, use the format \'pokemon-mega\'\n",
+              "For regional forms, use the format \'pokemon-alola\' or \'pokemon-galar\'\n",
+              "Replace any spaces or other punctuation with \'-\' (e.g. \'mr-mime\', \'nidoran-m\')\n",
+              "Find a full list of pokemon names in ./cache/pokemon_names.txt\n",
+              "Exiting.")
+        return
+    
+    is_cached = check_cache(api_pokemon_name, cache_dir)
+
+    if not is_cached: pull_pokedex_info(api_pokemon_name, cache_dir)
+    show_pokedex_info(api_pokemon_name, pretty_pokemon_name, cache_dir)
+
+    return
+
+if __name__ == "__main__":
+    main()
